25992f975a560e110b588bd47126ad4972eff84c
Rajiv-Stha <rajivnewar123@gmail.com>
2025-02-19 16:20:59 +0545

    map updated

--- frontend/src/components/contactUs/ContactUs.jsx
+++ frontend/src/components/contactUs/ContactUs.jsx
@@ -69,12 +69,13 @@ const ContactUs = () => {
                       initial="hidden"
                       whileInView={"show"}
                       viewport={{once:true, amount:0.7}}
-                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2632.200432686759!2d83.46650159303249!3d27.65793467051839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399684350f43c393%3A0x1e74d5ecc4f83d0f!2sSita%20Rice%20Mill%20(Sita%20Food%20Industries)!5e0!3m2!1sen!2snp!4v1736179200326!5m2!1sen!2snp" width="600" height="450" className={styles.map} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></motion.iframe>
+                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3533.105606311691!2d83.46155279999999!3d27.6831307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399687c23b2e2fb9%3A0x5dc818e35ffd0009!2sREYANSH%20INFOSYS!5e0!3m2!1sen!2snp!4v1739961277499!5m2!1sen!2snp" width="600" height="450" className={styles.map} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></motion.iframe>
                 </div>
             </div>
     </div>
 
     </div>
+    
       
     </>
   )
